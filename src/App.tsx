86e9fb55d834b89b6ec323dc86fff94ade1987d2
Beyn <mehmet.tokmak@beyn.com.tr>
2025-01-05 16:59:52 +0300

    Added service for http request

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios, { AxiosInstance } from 'axios';
 import './App.css'
+import ApiService from './service/request';
+
+
+
+// Axios instance oluştur
+const httpClient: AxiosInstance = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+  timeout: 3000,
+  headers: { 'Content-Type': 'application/json' },
+});
+const apiService = new ApiService(httpClient);
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('Mustafa')
+  useEffect(() => {
+    setTimeout(async () => {
+      setName('x2333')
+      const response = await apiService.fetchData();
+      console.log({ response });
+    }, 3000);
+  },)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-4">
+          <div className="alert alert-primary" role="alert">
+            {name}
+          </div>
+          <div className="alert alert-secondary" role="alert">
+            {name}
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="alert alert-success" role="alert">
+            {name}
+          </div>
+          <div className="alert alert-danger" role="alert">
+            {name}
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="alert alert-light" role="alert">
+            {name}
+          </div>
+          <div className="alert alert-dark" role="alert">
+            {name}
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+
   )
 }
 
